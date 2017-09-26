6b9ebaa683efbca2820a73499345aca1525949b3
Vedat Demir <vsahbaz@gmail.com>
2017-09-26 22:22:20 +0300

    commit 642

--- _site/js/back-to-top.js
+++ _site/js/back-to-top.js
@@ -2,7 +2,7 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
         document.getElementById("myBtn").style.display = "block";
     } else {
         document.getElementById("myBtn").style.display = "none";
@@ -10,7 +10,6 @@ function scrollFunction() {
 }
 
 // When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
+function topFunction() {  
+    $('html, body').animate({scrollTop: 0}, 300);
 }
