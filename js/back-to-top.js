c167dc45edc82e5f74b1a7df6c28d6a69d32202f
Vedat Demir <vsahbaz@gmail.com>
2017-09-26 22:17:03 +0300

    commit 639

--- js/back-to-top.js
+++ js/back-to-top.js
@@ -12,6 +12,5 @@ function scrollFunction() {
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
     document.body.animate({scrollTop: 0}, 300);
-    document.documentElement.animate({scrollTop: 0}, 300);   
-    $('html, body').animate({scrollTop: 0}, 300);
+    document.documentElement.animate({scrollTop: 0}, 300);  
 }
