d79fc3cd4e6c32a9c4a3592b5cbeaa9a0d957c8c
vdemir <vsahbaz@gmail.com>
2018-02-10 18:51:29 +0300

    1821

---
+++ vendor/bundle/ruby/2.3.0/gems/jekyll-assets-3.0.7/lib/jekyll-assets.rb
@@ -0,0 +1,6 @@
+# rubocop:disable Naming/FileName
+# Copyright: 2012 - 2018 - MIT License
+# Frozen-string-literal: true
+# Encoding: utf-8
+
+require_relative "jekyll/assets"
---
+++ vendor/bundle/ruby/2.3.0/gems/jekyll-assets-3.0.7/lib/jekyll/assets/plugins/font-awesome.rb
@@ -0,0 +1,8 @@
+# rubocop:disable Naming/FileName
+# Frozen-string-literal: true
+# Copyright: 2012 - 2018 - MIT License
+# Encoding: utf-8
+
+Jekyll::Assets::Utils.activate "font-awesome-sass" do
+  FontAwesome::Sass.load!
+end
